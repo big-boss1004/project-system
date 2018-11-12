big-boss1004/project-system

commit 77ca8fac8d423fbbfb1a43f5b1d71e0af3cb80f1

Remove duplicate collection access
First occurrence is a dictionary builder, where the indexer upserts.

Second occurrence is a set builder, where add upserts.

src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Tree/Dependencies/Snapshot/TargetedDependenciesSnapshot.cs
@@ -250,11 +250,10 @@ namespace Microsoft.VisualStudio.ProjectSystem.VS.Tree.Dependencies.Snapshot
 
                 anyChanges = true;
 
-                worldBuilder.Remove(newDependency.Id);
-                worldBuilder.Add(newDependency.Id, newDependency);
+                worldBuilder[newDependency.Id] = newDependency;
+
                 if (newDependency.TopLevel)
                 {
-                    topLevelBuilder.Remove(newDependency);
                     topLevelBuilder.Add(newDependency);
                 }
             }

commit 1dd495e65310b79eee62a3715a7453add88d48a7

Fix dependencies node regression
Fixes an error introduced in 77ca8fac8d423fbbfb1a43f5b1d71e0af3cb80f1
that caused many dependency tree nodes to appear with warning icons.

A unit test has been added to ensure this doesn't return.

The misconception in that earlier commit was that the Add method
replaces items that already present. There is no method on
ImmutableHashSet<>.Builder that achieves this, so we need the remove/add
combo.

src/Microsoft.VisualStudio.ProjectSystem.Managed.VS/ProjectSystem/VS/Tree/Dependencies/Snapshot/TargetedDependenciesSnapshot.cs
@@ -128,6 +128,7 @@ namespace Microsoft.VisualStudio.ProjectSystem.VS.Tree.Dependencies.Snapshot
 
                 if (newDependency.TopLevel)
                 {
+                    topLevelBuilder.Remove(newDependency);
                     topLevelBuilder.Add(newDependency);
                 }
             }

src/Microsoft.VisualStudio.ProjectSystem.Managed.VS.UnitTests/ProjectSystem/VS/Tree/Dependencies/Snapshot/TargetedDependenciesSnapshotTests.cs
@@ -3,6 +3,7 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Linq;
 
 using Microsoft.VisualStudio.Imaging;
 using Microsoft.VisualStudio.ProjectSystem.VS.Tree.Dependencies.CrossTarget;
@@ -536,6 +537,70 @@ namespace Microsoft.VisualStudio.ProjectSystem.VS.Tree.Dependencies
             Assert.True(snapshot.DependenciesWorld.ContainsKey(@"tfm1\xxx\addeddependency2"));
             Assert.True(snapshot.DependenciesWorld.ContainsKey(@"tfm1\xxx\InsteadRemoveddependency1"));
             Assert.True(snapshot.DependenciesWorld.ContainsKey(@"tfm1\xxx\addeddependency3"));
+        }
+
+        [Fact]
+        public void TFromChanges_UpdatesTopLevelDependencies()
+        {
+            const string projectPath = @"c:\somefolder\someproject\a.csproj";
+            var targetFramework = ITargetFrameworkFactory.Implement("tfm1");
+
+            var dependencyModelTopPrevious = IDependencyFactory.FromJson(@"
+            {
+                ""ProviderType"": ""Xxx"",
+                ""Id"": ""tfm1\\xxx\\topdependency1"",
+                ""Name"": ""TopDependency1"",
+                ""Caption"": ""TopDependency1"",
+                ""SchemaItemType"": ""Xxx"",
+                ""Resolved"": ""true""
+            }", icon: KnownMonikers.Uninstall, expandedIcon: KnownMonikers.Uninstall);
+
+            var dependencyModelTopAdded = IDependencyFactory.FromJson(@"
+            {
+                ""ProviderType"": ""Xxx"",
+                ""Id"": ""topdependency1"",
+                ""Name"": ""TopDependency1"",
+                ""Caption"": ""TopDependency1"",
+                ""SchemaItemType"": ""Xxx"",
+                ""Resolved"": ""true""
+            }", icon: KnownMonikers.Uninstall, expandedIcon: KnownMonikers.Uninstall);
+
+            var dependencyModelTopUpdated = IDependencyFactory.FromJson(@"
+            {
+                ""ProviderType"": ""Xxx"",
+                ""Id"": ""tfm1\\xxx\\topdependency1"",
+                ""Name"": ""TopDependency1"",
+                ""Caption"": ""TopDependency1"",
+                ""SchemaItemType"": ""Xxx"",
+                ""Resolved"": ""true""
+            }", icon: KnownMonikers.Uninstall, expandedIcon: KnownMonikers.Uninstall);
+
+            var catalogs = IProjectCatalogSnapshotFactory.Create();
+            var previousSnapshot = ITargetedDependenciesSnapshotFactory.Implement(
+                projectPath: projectPath,
+                targetFramework: targetFramework,
+                catalogs: catalogs,
+                dependenciesWorld: new Dictionary<string, IDependency>(StringComparer.OrdinalIgnoreCase),
+                topLevelDependencies: new[] { dependencyModelTopPrevious });
+
+            var changes = IDependenciesChangesFactory.Implement(
+                addedNodes: new[] { dependencyModelTopAdded },
+                removedNodes: Array.Empty<IDependencyModel>());
+
+            var snapshotFilter = new TestDependenciesSnapshotFilter()
+                    .ImplementBeforeAddResult(FilterAction.ShouldBeAdded, @"tfm1\xxx\topdependency1", dependencyModelTopUpdated);
+
+            var snapshot = TargetedDependenciesSnapshot.FromChanges(
+                projectPath,
+                previousSnapshot,
+                changes,
+                catalogs,
+                new[] { snapshotFilter },
+                new Dictionary<string, IProjectDependenciesSubTreeProvider>(),
+                null);
+
+            Assert.NotSame(previousSnapshot, snapshot);
+            Assert.Same(dependencyModelTopUpdated, snapshot.TopLevelDependencies.Single());
         }
 
         /// <summary>